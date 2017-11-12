6cb9e80b677f50b69de1b9ecc210b445a093c89d
Hexirp <Hexirp@users.noreply.github.com>
2017-11-12 16:51:20 +0900

    Add request in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,19 @@
 module Main where
  import Prelude
 
- import Network.HTTP.Simple (httpLBS, getResponseBody)
- import Text.HTML.DOM (parseLBS)
+ import Data.ByteString.Lazy.Char8 (ByteString)
+ import Network.HTTP.Simple (httpLBS, getResponseBody, Response)
+ import Text.HTML.DOM
  import Text.XML.Cursor
 
  main :: IO ()
  main = do
-  res <- httpLBS "https://github.com/trending/haskell"
+  res <- request
   print $ (fromDocument $ parseLBS $ getResponseBody res)
    $// element "h3"
    >=> child
    >=> element "a"
    >=> attribute "href"
+
+ request :: IO (Response ByteString)
+ request = httpLBS "https://github.com/trending/haskell"
