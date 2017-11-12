299c0eb3e14423faa66297468205fc61fbc7a399
Hexirp <Hexirp@users.noreply.github.com>
2017-11-12 15:56:28 +0900

    Update Main.hs to scrape trends

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,13 @@ module Main where
 
  import Network.HTTP.Simple (httpLBS, getResponseBody)
  import Text.HTML.DOM (parseLBS)
+ import Text.XML.Cursor
 
  main :: IO ()
  main = do
   res <- httpLBS "https://github.com/trending/haskell"
-  print $ parseLBS $ getResponseBody res
+  print $ (fromDocument $ parseLBS $ getResponseBody res)
+   $// element "h3"
+   >=> child
+   >=> element "a"
+   >=> attribute "href"
