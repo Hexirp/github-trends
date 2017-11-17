4fa14d42f1820c98f94ffc386cf5fdfb70f3b5f3
Hexirp <Hexirp@users.noreply.github.com>
2017-11-17 21:24:50 +0900

    Clean GitHubTrends.hs

--- GitHubTrends.hs
+++ GitHubTrends.hs
@@ -5,13 +5,14 @@ module Main where
 
  import Data.ByteString.Lazy.Char8 (ByteString)
  import Data.Text (Text, intercalate, append, pack, unpack)
+
  import Network.HTTP.Simple (httpLBS, getResponseBody)
  import Text.XML (Document)
  import Text.XML.Cursor
  import Text.HTML.DOM (parseLBS)
 
  main :: IO ()
- main = request >>= putStr . unpack . format . scrape . parseLBS
+ main = request >>= putStr . unpack . make . format . scrape . parseLBS
 
  request :: IO ByteString
  request = getResponseBody <$> httpLBS "https://github.com/trending/haskell"
@@ -27,8 +28,9 @@ module Main where
  format x = intercalate "\\n"
   $ zipWith append
    (pack <$> (++ ". ") <$> show <$> [1 :: Int .. ])
-   $ zipWith id
-    (replicate 3 (sandwich "*" "*") ++ repeat id)
-    (sandwich "<" ">" <$> append "https://github.com" <$> x)
-  where
-   sandwich a b c = a `append` c `append` b
+   (sandwich "<" ">" <$> append "https://github.com" <$> x)
+
+ make :: Text -> Text
+ make = sandwich "[{\"text\": \"" "\", \"}]"
+
+ sandwich a b c = a `append` c `append` b
