b90c756860ef29123a4a8556427eb785ebad1d1d
Hexirp <Hexirp@users.noreply.github.com>
2017-11-23 16:43:08 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+TRENDS=$(stack exec github-trends)
 curl -XPOST \
   -d "token=$1" \
   -d "channel=@hexirp" \
+  -d "as_user=false" \
+  -d "username=GitHub Trends" \
   -d "text=Today's GitHub trends!" \
-  -d "attachments=$(stack exec github-trends)" \
-  "https://slack.com/api/chat.postMessage"
+  -d "attachments=${TRENDS}" \
+  "https://slack.com/api/chat.postMessage" \
+    &> /dev/null
